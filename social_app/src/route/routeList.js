d9cfca7032115f22e9c684fca80f199189c08dd4
sumreen ali <sumreen.ali@hcsgroup.io>
2022-03-09 01:15:22 +0500

    initial home component added

--- social_app/src/route/routeList.js
+++ social_app/src/route/routeList.js
@@ -1,8 +1,10 @@
-import {Splash, Signup, Login,} from '../screens';
+import {Splash, Signup, Login, Home} from '../screens';
 
 
 export const stackRouteList = [
     { name: "splash", component: Splash },
     { name: "signup", component: Signup },
     { name: "login", component: Login },
+    { name: "home", component: Home },
+
 ]
--- social_app/src/screens/index.js
+++ social_app/src/screens/index.js
@@ -1,3 +1,4 @@
 export {default as Login} from './login'
 export{ default as Signup} from './signup'
 export {default as Splash} from './splash'
+export {default as Home} from './home'
